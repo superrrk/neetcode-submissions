687bcc868fee08819566cb00f4275ac0667e0265
Kelly Truong <89561578+superrrk@users.noreply.github.com>
2026-07-15 00:28:30 -0500

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,100 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # convert string to all lowercase alphanumeric chars
+        # two pointer to check if string reversed 
+        
+        # edge case: a str with <1 or 0 char is a palindrome
+        if len(s) <= 1:
+            return True
+        
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            # clean the string by skipping all alnum chars
+            while l < r and not s[l].isalnum():
+                l += 1
+            
+            while l < r and not s[r].isalnum():
+                r -= 1
+            
+            # if the l and r char are diff, return false 
+            if s[l].lower() != s[r].lower(): 
+                return False
+            # move onto the next set of chars, L and R pair
+            l += 1
+            r -= 1
+        return True
+
+
+
+            
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # parse through the string using a left and r poitner to see if they are the same
+    # the whole string must be converted to lowercase
+    # and remove all the spaces, so that we are just comparing the letters
+    # the letter on the l and r pointer must be the same letter, 
+    # return true if this is the case for the whole string
+    # false otherwise
+    
+    # edge caes: if a string is empty, return false
+
+    # brute force approach is to make a copy of the string
+    # clean the string by checking for alphanumeric char, (a space is not alphanum)
+    # then reverse the new string it to see if they are equal
+    
+        # cleanedString = ""
+        # for character in s: 
+        #     if character.isalnum():
+        #         cleanedString += character.lower()
+        # return cleanedString == cleanedString[::-1]
+    # Time O(n): loops through each character n times, where n is the average length of the string s
+    # Space O(n): created a new string for each character
+
+        l, r = 0, len(s) - 1
+        
+        while l < r: 
+            # checking if valid char/digit, removing spaces, punctuation, and special chars
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            # if they're different letters, immediately return false
+            if s[l].lower() != s[r].lower():
+                return False
+            # keep incrementing, decrementing 
+            l, r = l + 1, r - 1
+        return True
+
+    def alphaNum(self, c):
+        return (ord("a") <= ord(c) < ord("z") or 
+                ord("A") <= ord(c) < ord("Z") or
+                ord("0") <= ord(c) < ord("9"))
+            
+
