0c0a56a00334cafd0fdaa4cbef8baf929fcd02e9
Kelly Truong <89561578+superrrk@users.noreply.github.com>
2026-07-15 22:07:19 -0500

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.py
@@ -0,0 +1,28 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # want to find two digits that equal the target
+        # brute force: use nested for loop to track the sum of 2 digits
+        # optimal: use a hashmap to store the differences and 
+        # check if any number + the diff add up to the target
+
+
+
+        seen = {}
+        
+        for i, num in enumerate(nums):
+            diff = target - num 
+            if diff in seen: 
+                return [seen[diff], i]
+            seen[num] = i
+
+
+
+
+
+            
+
+
+            
+
+        
+
