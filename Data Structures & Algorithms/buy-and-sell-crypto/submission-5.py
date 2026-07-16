4a99baf193951708f02354767fbcd2ad1cc6e08c
Kelly Truong <89561578+superrrk@users.noreply.github.com>
2026-07-15 23:37:21 -0500

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # find the two days where the difference in price is the highest
+        # two pointer approach
+
+        l, r = 0, 1
+        max_profit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
+            else: 
+                l = r
+            r += 1
+        
+        return max_profit
+
+
