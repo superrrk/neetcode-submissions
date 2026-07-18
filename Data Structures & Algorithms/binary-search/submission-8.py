92a9535b5ae4694f86ced5af74d3eb62aac7b033
Kelly Truong <89561578+superrrk@users.noreply.github.com>
2026-07-17 21:33:06 -0500

    Add: binary-search - submission-8

---
+++ Data Structures & Algorithms/binary-search/submission-8.py
@@ -0,0 +1,83 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # find the midpoint
+        # set r pointer to mid, if target in left side
+        # set l pointer to mid if target in r side
+        # else, target found
+
+        l, r = 0, len(nums) - 1
+
+        while l <= r: 
+            mid = l + (r - l) // 2
+            
+            if target < nums[mid]:
+                r = mid - 1
+            elif target > nums[mid]:
+                l =  mid + 1
+            else: 
+                return mid
+        return -1
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # target not in nums --> - 1
+        # target found --> index
+
+        # array empty? --> no, at least 1 number
+
+        # bst 
+        # [ -1, 0, 2, 4, 6, 8]
+        # [ l                r]
+        # 0 + 5 // 2 = 2 
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = l + ((r - l) // 2) # middle index
+            
+            if nums[mid] < target: 
+                l = mid + 1
+            
+            elif nums[mid] > target:
+                r = mid - 1
+
+            else: 
+                return mid
+                
+        return -1
+
+
+
+
+            
+
