fa5ca83fabc12a755b8e88965390124b511a132b
Kelly Truong <89561578+superrrk@users.noreply.github.com>
2026-07-25 16:57:31 -0500

    Add: combination-target-sum-ii - submission-7

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-7.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        result = []
+        
+
+        def generate_subsets(i, currList, total): 
+            if total == target:
+                result.append(currList.copy()) # save sublist
+                return
+            
+            #skip candidate
+            if total > target or i == len(candidates): 
+                return
+            
+            # add candidates[i]
+            currList.append(candidates[i])
+            generate_subsets(i + 1, currList, total + candidates[i])
+            # do not add candidates[i]
+            currList.pop()
+
+            while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
+                i += 1
+            generate_subsets(i + 1, currList, total)
+    
+        generate_subsets(0, [], 0)
+        return result
+
+
