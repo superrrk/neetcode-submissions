5ab8503cd22a91c5692726a2bb4368286f728881
Kelly Truong <89561578+superrrk@users.noreply.github.com>
2026-08-07 15:20:36 -0500

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        # expected is the increasing order of the heights array
+        # heights is the actual order
+        # return the number of mismatched heights
+
+        # sort the heights array, then compare the original arr to this
+        # for any heights that don't match, add one to the index
+        indices = 0
+
+        expected = sorted(heights)
+
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                indices += 1
+
+        return indices
+            
+
+        
