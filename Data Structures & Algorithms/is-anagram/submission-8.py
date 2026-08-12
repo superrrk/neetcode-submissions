f105b100aa3f20a0d91ae347765440b504212031
Kelly Truong <89561578+superrrk@users.noreply.github.com>
2026-08-12 15:25:50 -0500

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,63 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # check str length if equal
+        # compare if freq of letter is each teh same
+        if len(s) != len(t):
+            return False
+
+        seen_s = {}
+        seen_t = {}
+
+        for i in range(len(s)):
+            seen_s[s[i]] = seen_s.get(s[i], 0) + 1
+            seen_t[t[i]] = seen_t.get(t[i], 0) + 1
+            
+        return seen_s == seen_t
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     # both strings need to have the exact same length
+        # both strings need to have the same freq of each letters
+        '''
+        hashmap to store the freq of each letter
+        a : 2 
+        letter : number of times it appears
+
+        hashmap(s) == hashmap(t) 
+        '''
+
+        if len(s) != len(t): 
+            return False
+        
+        countS = {}
+        countT = {}
+
+        for i in range(len(s)): 
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
